7c99e8060a5ce15235d9041c3ce30da6ec83f62c
Nurgeldi <nurekesadykov4@gmail.com>
2021-07-18 15:14:03 +0600

    Commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DemoApplicationTests {
+public class DemoApplicationTests implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run ( DemoApplicationTests.class , args );
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
---
+++ src/test/java/com/example/demo/User.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String surname;
+}
